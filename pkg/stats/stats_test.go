9f3303440e929bd3d59de8e6b75bacf8fd09b2ea
Sunbula <khasanovasumbula@gmail.com>
2021-10-05 13:28:44 +0500

    ch

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -8,83 +8,83 @@ import (
 	
 )
 
-// func TestFilterByCategory_nil(t *testing.T) {
-// 	var payments []types.Payment
+func TestFilterByCategory_nil(t *testing.T) {
+	var payments []types.Payment
 
-// 	result := FilterByCategory(payments, "mobile")
+	result := FilterByCategory(payments, "mobile")
 
-// 	if len(result) != 0{
-// 		t.Error("resul len != 0")
-// 	}
-// }
+	if len(result) != 0{
+		t.Error("resul len != 0")
+	}
+}
 
-// func TestFilterByCategory_empty(t *testing.T) {
-// 	payments := []types.Payment{}
+func TestFilterByCategory_empty(t *testing.T) {
+	payments := []types.Payment{}
 
-// 	result := FilterByCategory(payments, "mobile")
+	result := FilterByCategory(payments, "mobile")
 
-// 	if len(result) !=0 {
-// 		t.Error("result len != 0")
-// 	}
+	if len(result) !=0 {
+		t.Error("result len != 0")
+	}
 
-// }
+}
 
-// func TestFilterByCategory_notFound(t *testing.T) {
-// 	payments := []types.Payment{
-// 		{ID: 1, Category: "auto"},
-// 		{ID: 2, Category: "food"},
-// 		{ID: 3, Category: "auto"},
-// 		{ID: 4, Category: "auto"},
-// 		{ID: 5, Category: "fun"},
-// 	}
+func TestFilterByCategory_notFound(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 2, Category: "food"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+		{ID: 5, Category: "fun"},
+	}
 
-// 	result := FilterByCategory(payments, "mobile")
+	result := FilterByCategory(payments, "mobile")
 
-// 	if len(result) != 0{
-// 		t.Error("result len != 0")
-// 	}
-// }
+	if len(result) != 0{
+		t.Error("result len != 0")
+	}
+}
 
-// func TestFilterByCategory_foundOne(t *testing.T) {
-// 	payments := []types.Payment{
-// 		{ID: 1, Category: "auto"},
-// 		{ID: 2, Category: "food"},
-// 		{ID: 3, Category: "auto"},
-// 		{ID: 4, Category: "auto"},
-// 		{ID: 5, Category: "fun"},
-// 	}
-// 	expected := []types.Payment{
-// 		{ID: 2, Category: "food"},
-// 	}
+func TestFilterByCategory_foundOne(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 2, Category: "food"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+		{ID: 5, Category: "fun"},
+	}
+	expected := []types.Payment{
+		{ID: 2, Category: "food"},
+	}
 
-// 	result := FilterByCategory(payments, "food")
+	result := FilterByCategory(payments, "food")
 
-// 	if !reflect.DeepEqual(expected, result){
-// 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
-// 	}
-// }
+	if !reflect.DeepEqual(expected, result){
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
 
-// func TestFilterByCategory_moreThanOne(t *testing.T) {
-// 	payments := []types.Payment{
-// 		{ID: 1, Category: "auto"},
-// 		{ID: 2, Category: "food"},
-// 		{ID: 3, Category: "auto"},
-// 		{ID: 4, Category: "auto"},
-// 		{ID: 5, Category: "fun"},
-// 	}
+func TestFilterByCategory_moreThanOne(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 2, Category: "food"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+		{ID: 5, Category: "fun"},
+	}
 
-// 	expected := []types.Payment{
-// 		{ID: 1, Category: "auto"},
-// 		{ID: 3, Category: "auto"},
-// 		{ID: 4, Category: "auto"},
-// 	}
+	expected := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+	}
 
-// 	result := FilterByCategory(payments, "auto")
+	result := FilterByCategory(payments, "auto")
 
-// 	if !reflect.DeepEqual(expected, result){
-// 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
-// 	}
-// }
+	if !reflect.DeepEqual(expected, result){
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
 
 func TestCategoriesAvg(t *testing.T) {
 	payments := []types.Payment{
